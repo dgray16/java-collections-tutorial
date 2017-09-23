90f22accf5622e27fa418f22f7710c68c5ce7b4c
Dgray16 <vova235@gmail.com>
2017-09-23 08:17:44 +0300

    Implement TreeMap sample.

--- src/com/chnu/java/collections/list/TreeMapSample.java
+++ src/com/chnu/java/collections/list/TreeMapSample.java
@@ -1,9 +1,21 @@
 package com.chnu.java.collections.list;
 
+import com.chnu.java.Dog;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TreeMapSample {
 
     public void treeMap() {
 
+        TreeMap<Integer, Dog> treeMap = new TreeMap<>();
+
+        for (int i = 5; i > 0; i--) {
+            treeMap.put(i, new Dog(i));
+        }
+
+        System.out.println("End");
     }
 
 }
